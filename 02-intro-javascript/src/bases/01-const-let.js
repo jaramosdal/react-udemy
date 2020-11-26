8001486eb0dd59047c1a4032188efce0b76c177e
Javier Ramos Nodal <javiramos@soliss.com>
2020-11-26 17:33:45 +0100

    13

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,19 @@
+// Variables y Constantes
+
+const nombre = 'Javi';
+const apellido = 'Ramos';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+// var No se debe de usar...
+if (true) {
+    const nombre = 'Peter';
+    let valorDado = 6;
+
+    console.log(nombre, valorDado);
+}
+
+console.log(valorDado);

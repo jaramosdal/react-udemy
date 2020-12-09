9e235df06a9167de1d3847792790b33451484439
Javier Ramos Nodal <javiramos@soliss.com>
2020-12-10 00:48:33 +0100

    72

--- 04-gif-expert-app/src/GifExpertApp.js
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -1,22 +1,21 @@
 import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
 
 const GifExpertApp = () => {
 
-    // const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
     const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball'])
 
-    const handleAdd = () => {
-        // setCategories([...categories, 'Otra']);
-        // el callback recibe el estado anterior y devuelve el nuevo
-        setCategories(cats => [...cats, 'Otra']);
-    }
+    // const handleAdd = () => {
+    //     // setCategories([...categories, 'Otra']);
+    //     // el callback recibe el estado anterior y devuelve el nuevo
+    //     setCategories(cats => [...cats, 'Otra']);
+    // }
 
     return (
         <>
             <h2>GifExpertApp</h2>
+            <AddCategory setCategories={ setCategories } />
             <hr />
-
-            <button onClick={handleAdd}>Agregar</button>
 
             <ol>
             {

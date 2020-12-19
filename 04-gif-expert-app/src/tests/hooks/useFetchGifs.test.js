427be31b40386054197d29e0f4496c73b47b3c38
Javier Ramos Nodal <javiramos@soliss.com>
2020-12-19 23:40:05 +0100

    Sección 8:Testing - Probando la aplicación de GifExpert

--- 04-gif-expert-app/src/tests/hooks/useFetchGifs.test.js
+++ 04-gif-expert-app/src/tests/hooks/useFetchGifs.test.js
@@ -1,16 +1,27 @@
 import React from 'react'
-import { shallow } from 'enzyme';
 import {useFetchGifs} from '../../hooks/useFetchGifs';
 import { renderHook } from '@testing-library/react-hooks';
 
 describe('Pruebas en el hook useFetchGifs', () => {
 
-    test('debe retornar el estado inicial', () => {
-        const { result } = renderHook( () => useFetchGifs('One Punch') );
+    test('debe retornar el estado inicial', async () => {
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs('One Punch') );
         const { data, loading } = result.current;
         
+        await waitForNextUpdate();
+
         expect(data).toEqual([]);
         expect(loading).toBe(true);
+    });
+
+    test('debe retornar un array de imágenes y el loading en false', async () => {
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs('One Punch') );
+        await waitForNextUpdate();
+
+        const { data, loading } = result.current;
+        
+        expect(data.length).toBe(10);
+        expect(loading).toBe(false);
     });
 
 });

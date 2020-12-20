5d264526067f8bd4a008514c10517aaba877cb1f
Javier Ramos Nodal <javiramos@soliss.com>
2020-12-20 01:51:42 +0100

    104

---
+++ 05-hook-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HooApp } from './HooApp';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <HooApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);

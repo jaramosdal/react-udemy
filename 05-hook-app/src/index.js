062b679d37d7acd5cb782f892c38cb0d5de741f0
Javier Ramos Nodal <javiramos@soliss.com>
2020-12-20 15:05:34 +0100

    107

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HooApp } from './HooApp';
+// import { HookApp } from './HookApp';
+// import { CounterApp } from './components/01-useState/CounterApp';
+// import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+import { SimpleForm } from './components/02-useEffect/SimpleForm';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HooApp />
+    <SimpleForm />
   </React.StrictMode>,
   document.getElementById('root')
 );

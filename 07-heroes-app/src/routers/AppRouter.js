d30baae2e8016481ad510ac338566bd570aad435
Javier Ramos Nodal <javiramos@soliss.com>
2020-12-29 21:35:46 +0100

    171

---
+++ 07-heroes-app/src/routers/AppRouter.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { LoginScreen } from "../components/login/LoginScreen";
+import { Marvel } from "../components/marvel/Marvel";
+import { Navbar } from "../components/ui/NavBar";
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+
+        <Switch>
+            <Route exact path="/Login" component={ LoginScreen }/>
+            <Route exact path="/" component={ Marvel }/>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default AppRouter;

86de6f5c3f2fb8272b679c8904d3b0d346c5f736
Javier Ramos Nodal <javiramos@soliss.com>
2020-12-31 13:11:18 +0100

    189

--- 07-heroes-app/src/components/login/LoginScreen.js
+++ 07-heroes-app/src/components/login/LoginScreen.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../types/types';
 
 // history es una propiedad que me provee de manera automática el Router
 export const LoginScreen = ({ history }) => {
 
+    const { dispatch } = useContext(AuthContext);
+
     const handleLogin = () => {
         // history.push('/');
+        
+        const action = {
+            type: types.login,
+            payload: {
+                name: 'Javi'
+            }
+        }
+        dispatch(action);
+        
         history.replace('/');
     }
 

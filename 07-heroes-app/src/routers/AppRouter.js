a29c4b238f33e220af58b2a48f2b9a9d8ba3c3cb
Javier Ramos Nodal <javiramos@soliss.com>
2021-01-01 14:52:03 +0100

    192

--- 07-heroes-app/src/routers/AppRouter.js
+++ 07-heroes-app/src/routers/AppRouter.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { AuthContext } from "../auth/AuthContext";
 import { LoginScreen } from "../components/login/LoginScreen";
 import { DashboardRoutes } from "./DashboardRoutes";
+import { PrivateRoute } from "./PrivateRoute";
 
 const AppRouter = () => {
+
+  const { user } = useContext(AuthContext);
+
   return (
     <Router>
       <div>
         <Switch>
             <Route exact path="/Login" component={ LoginScreen }/>
-            <Route path="/" component={ DashboardRoutes }/>
+            <PrivateRoute 
+              isAuthenticated={ user.logged }
+              path="/" 
+              component={ DashboardRoutes }
+            />
         </Switch>
       </div>
     </Router>

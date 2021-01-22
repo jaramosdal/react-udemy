e2a175dcbbda62ca645697ca4c949f8598351fe7
Javier Ramos Nodal <javiramos@soliss.com>
2021-01-22 19:24:49 +0100

    Sección 17:JournalApp - SASS - Estructura y Diseño

---
+++ 08-journal-app/src/components/journal/JournalEntry.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export const JournalEntry = () => {
+    return (
+        <div className="journal__entry pointer">
+            <div 
+                className="journal__entry-picture"
+                style={{
+                    backgroundSize: 'cover',
+                    backgroundImage: 'url(https://fundaciongabo.org/sites/default/files/styles/850px/public/imagenes-falsas.jpg?itok=QYSpPSZw)'
+                }}
+            ></div>
+            <div className="journal__entry-body">
+                <p className="journal__entry-title">
+                    Un nuevo día
+                </p>
+                <p className="journal__entry-content">
+                    dfbaewrevergqerber e ete rg rg fg sfgsg
+                </p>
+            </div>
+
+            <div className="journal__entry-date-box">
+                <span>Monday</span>
+                <h4>28</h4>
+            </div>
+        </div>
+    )
+}

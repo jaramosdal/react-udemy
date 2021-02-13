6ddc8e55facbb1122ba518a82f5de8cd37e77fea
Javier Ramos Nodal <javiramos@soliss.com>
2021-02-13 19:01:04 +0100

    Sección 24: Backend - Eventos del calendario - CRUD

--- 10-calendar-backend/controllers/events.js
+++ 10-calendar-backend/controllers/events.js
@@ -6,10 +6,14 @@
 const { response } = require("express");
 const Evento = require('../models/Evento')
 
-const getEventos = (req, res = response) => {
+const getEventos = async(req, res = response) => {
+    const eventos = await Evento.find()
+                                .populate('user', 'name');
+                                // .populate('user', 'name email');
+
     res.json({
         ok: true,
-        msg: 'getEventos'
+        msg: eventos
     });
 }
 
@@ -19,7 +23,7 @@ const crearEvento = async(req, res = response) => {
     
     try {
         evento.user = req.uid;
-        
+
         const eventoGuardado = await evento.save();
 
         res.json({
@@ -35,18 +39,81 @@ const crearEvento = async(req, res = response) => {
     }
 }
 
-const actualizarEvento = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'actualizarEvento'
-    });
+const actualizarEvento = async(req, res = response) => {
+
+    const eventoId = req.params.id;
+    const { uid } = req;
+
+    try {
+        const evento = await Evento.findById(eventoId);
+
+        if (!evento) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Evento no existe por ese id'
+            })
+        }
+
+        if (evento.user.toString() !== uid) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene privilegio de editar este evento'
+            })      
+        }
+
+        const nuevoEvento = {
+            ...req.body,
+            user: uid
+        }
+
+        const eventoActualizado = await Evento.findByIdAndUpdate(eventoId, nuevoEvento, { new: true });
+
+        res.json({
+            ok: true,
+            evento: eventoActualizado
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        });
+    }
 }
 
-const eliminarEvento = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'eliminarEvento'
-    });
+const eliminarEvento = async(req, res = response) => {
+    const eventoId = req.params.id;
+    const uid = req.uid;
+
+    try {
+        const evento = await Evento.findById(eventoId);
+
+        if (!evento) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Evento no existe por ese id'
+            })
+        }
+
+        if (evento.user.toString() !== uid) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene privilegio de eliminar este evento'
+            })      
+        }
+
+        await Evento.findOneAndDelete(eventoId);
+
+        res.json({ ok: true })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        });
+    }
 }
 
 
--- 10-calendar-backend/routes/events.js
+++ 10-calendar-backend/routes/events.js
@@ -33,7 +33,16 @@ router.post(
 );
 
 // Actualizar evento
-router.put('/:id', actualizarEvento)
+router.put(
+    '/:id', 
+    [
+        check('title','El titulo es obligatorio').not().isEmpty(),
+        check('start','Fecha de inicio es obligatoria').custom( isDate ),
+        check('end','Fecha de finalización es obligatoria').custom( isDate ),
+        validarCampos
+    ],
+    actualizarEvento 
+);
 
 // Actualizar evento
 router.delete('/:id', eliminarEvento)
